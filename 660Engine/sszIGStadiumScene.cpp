db3eb72991ad7b1d685cec846bde327b91af5a47
CHNXIK <chnxik@gmail.com>
2023-07-10 02:35:37 +0900

    1. IGStadium 추가

--- 660Engine/sszIGStadiumScene.cpp
+++ 660Engine/sszIGStadiumScene.cpp
@@ -35,52 +35,26 @@ namespace ssz
 	{
 #pragma region Make Material for this Scene
 		{
-			// Get Using Texture
-			Resources::Load<Texture>(L"BattleHeaderTex", L"..\\Resources\\useResource\\GameScene\\header_bg.png");
 
-			// Make Material
-//			std::shared_ptr<Material> BattleHeader_Mt = std::make_shared<Material>();
-//			BattleHeader_Mt->SetShader(SpriteShader);
-//			BattleHeader_Mt->SetTexture(BattleHeaderTex);
-//			BattleHeader_Mt->SetRenderingMode(eRenderingMode::Transparent);
-//			Resources::Insert(L"BattleHeaderMt", BattleHeader_Mt);
 		}
 #pragma endregion
 #pragma region Create Object for this Scene
 		// GameObject
 		{
 			// Battle Header
-			GameObject* BattleHeaderBg = new GameObject();
+			GameObject* BattleHeaderBg = Instantiate<GameObject>(Vector3(0.0f, 516.f, 1.04f), Vector3(1920.f, 123.f, 1.f), eLayerType::BackGround);
 			BattleHeaderBg->SetName(L"BattleHeaderBg");
-			AddGameObject(eLayerType::BackGround, BattleHeaderBg);
-
-			MeshRenderer* BattleHeaderBg_mr = BattleHeaderBg->AddComponent<MeshRenderer>();
-			BattleHeaderBg_mr->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
-			BattleHeaderBg_mr->SetMaterial(Resources::Find<Material>(L"BattleHeaderMt"));
-			BattleHeaderBg->GetComponent<Transform>()->SetPosition(Vector3(0.0f, 0.4785f, 1.04f));
-			BattleHeaderBg->GetComponent<Transform>()->SetScale(Vector3(1.92f, 0.123f, 1.f));
+			BattleHeaderBg->AddComponent<MeshRenderer>()->SetMeshRenderer(L"RectMesh", L"BattleHeaderMt");
 
 			// IG Stadium
-			GameObject* IG_Stadium = new GameObject();
-			IG_Stadium->SetName(L"Stadium");
-			AddGameObject(eLayerType::BackGround, IG_Stadium);
-
-			MeshRenderer* IG_Stadium_Mr = IG_Stadium->AddComponent<MeshRenderer>();
-			IG_Stadium_Mr->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
-			IG_Stadium_Mr->SetMaterial(Resources::Find<Material>(L"IGStadiumMt"));
-			IG_Stadium->GetComponent<Transform>()->SetPosition(Vector3(0.0f, -0.119f, 1.11f));
-			IG_Stadium->GetComponent<Transform>()->SetScale(Vector3(2.235f, 1.46f, 1.f));
+			GameObject* IG_Stadium = Instantiate<GameObject>(Vector3(0.0f, -44.0f, 1.021f), Vector3(2235.f, 1460.f, 1.f), eLayerType::BackGround);
+			IG_Stadium->SetName(L"IG_Stadium");
+			IG_Stadium->AddComponent<MeshRenderer>()->SetMeshRenderer(L"RectMesh", L"IGStadiumMt");
 
 			// IG StadiumSky
-			GameObject* IG_StadiumSky = new GameObject();
-			IG_StadiumSky->SetName(L"StadiumSky");
-			AddGameObject(eLayerType::BackGround, IG_StadiumSky);
-
-			MeshRenderer* IG_StadimSky_mr = IG_StadiumSky->AddComponent<MeshRenderer>();
-			IG_StadimSky_mr->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
-			IG_StadimSky_mr->SetMaterial(Resources::Find<Material>(L"IGStadiumSkyMt"));
-			IG_StadiumSky->GetComponent<Transform>()->SetPosition(Vector3(0.0f, -0.136f, 1.12f));
-			IG_StadiumSky->GetComponent<Transform>()->SetScale(Vector3(2.144f, 1.429f, 1.f));
+			GameObject* IG_StadiumSky = Instantiate<GameObject>(Vector3(0.0f, -53.0f, 1.022f), Vector3(2144.f, 1429.f, 1.f), eLayerType::BackGround);
+			IG_StadiumSky->SetName(L"IG_StadiumSky");
+			IG_StadiumSky->AddComponent<MeshRenderer>()->SetMeshRenderer(L"RectMesh", L"IGStadiumSkyMt");
 
 			// 오브젝트 배치용 스크립트
 			ArrangementScript* ArScript = IG_Stadium->AddComponent<ArrangementScript>();

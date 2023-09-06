7cd5f690d5be32902f3031100ed3131cbab9ff05
CHNXIK <chnxik@gmail.com>
2023-09-06 15:04:35 +0900

    새 게임 메뉴 이름 작성

--- 660Engine/sszNewGameWindow.cpp
+++ 660Engine/sszNewGameWindow.cpp
@@ -37,6 +37,24 @@ namespace ssz
 			// Set default Size
 			Transform* tr = GetComponent<Transform>();
 			tr->SetScale(Vector3(1350.f, 786.f, 1.f));
+
+			Text* NewGameTitle = AddComponent<Text>();
+			Text* TeamName = AddComponent<Text>();
+			Text* CoachName = AddComponent<Text>();
+			Text* TeamLogo = AddComponent<Text>();
+			Text* CoachTex = AddComponent<Text>();
+
+			NewGameTitle->TextInit(L"Galmuri14", Vector3(0.f, 350.f, 0.f), 50.f, FONT_RGBA(255, 255, 255, 255), FW1_CENTER);
+			TeamName->TextInit(L"Galmuri14", Vector3(-330.f, 250.f, 0.f), 40.f, FONT_RGBA(255, 255, 255, 255), FW1_CENTER);
+			CoachName->TextInit(L"Galmuri14", Vector3(340.f, 250.f, 0.f), 40.f, FONT_RGBA(255, 255, 255, 255), FW1_CENTER);
+			TeamLogo->TextInit(L"Galmuri14", Vector3(-330.f, 30.f, 0.f), 40.f, FONT_RGBA(255, 255, 255, 255), FW1_CENTER);
+			CoachTex->TextInit(L"Galmuri14", Vector3(340.f, 30.f, 0.f), 40.f, FONT_RGBA(255, 255, 255, 255), FW1_CENTER);
+
+			NewGameTitle->SetString(L"새 게임");
+			TeamName->SetString(L"팀 이름");
+			CoachName->SetString(L"감독 이름");
+			TeamLogo->SetString(L"팀 로고");
+			CoachTex->SetString(L"감독 외형");
 		}
 #pragma endregion
 	}
